a932443bb1bb0d438eeef23ba2f32c755a006bf7
Marcus Sanatan <msanatan@gmail.com>
2021-06-27 16:30:13 -0400

    Create a page for blog posts

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/Layout'
+
+const BlogPage = () => {
+  return (
+    <Layout pageTitle='Blog'>
+      <h1>Welcome to my blog</h1>
+    </Layout>
+  )
+}
+
+export default BlogPage
