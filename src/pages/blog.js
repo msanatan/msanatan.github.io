c769a458f7947724c58f42447b63e4ef6c76763f
Marcus Sanatan <msanatan@gmail.com>
2021-06-28 09:06:49 -0400

    Pull list of markdown files and render them in a list
    Just a beginning to having blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,33 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
+import { pageTitle } from '../styles/page.module.css'
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle='Blog'>
-      <h1>Welcome to my blog</h1>
+      <h1 className={pageTitle}>Blog</h1>
+      <ul>
+        {
+          data.allFile.nodes.map(node => (
+            <li key={node.name}>
+              {node.name}
+            </li>
+          ))
+        }
+      </ul>
     </Layout>
   )
 }
 
+export const query = graphql`
+  query AllBlogPosts {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`
+
 export default BlogPage
- 
