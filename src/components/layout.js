226bef9a74186371028a60e5f7dcc5b4a9238631
Marcus Sanatan <msanatan@gmail.com>
2021-06-27 23:38:38 -0400

    Fix navbar toggle and fix main content spacing
    We couldn't use the main tag because of its style.
    We can create a CSS class to remove the spacing, then apply row etc.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Link, graphql, useStaticQuery } from 'gatsby'
+import { menuItem } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
   const data = useStaticQuery(graphql`
@@ -16,30 +17,28 @@ const Layout = ({ pageTitle, children }) => {
   const siteTitleText = data.site.siteMetadata.title
 
   return (
-    <div className='container w-100 h-100'>
+    <div className='container h-100 d-flex flex-column'>
       <div className='row'>
         <header>
           <nav className='navbar navbar-expand-lg navbar-light'>
-            <button className='navbar-toggler' type='button'>
+            <button className='navbar-toggler' type='button' data-bs-toggle="collapse" data-bs-target="#navBarTogglerMenu" aria-controls="navBarTogglerMenu" aria-expanded="false" aria-label="Toggle navigation">
               <span className='navbar-toggler-icon'></span>
             </button>
-            <div className='collapse navbar-collapse' id='navBarTogglerMenu'>
-              <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+            <div className='collapse navbar-collapse justify-content-lg-center' id='navBarTogglerMenu'>
+              <ul className='navbar-nav mr-auto mb-2 mb-lg-0'>
                 <li className='nav-item'>
-                  <Link to='/' className='nav-link'>Home</Link>
+                  <Link to='/' className={`nav-link ${menuItem}`}>Home</Link>
                 </li>
                 <li className='nav-item'>
-                  <Link to='/blog' className='nav-link'>Blog</Link>
+                  <Link to='/blog' className={`nav-link ${menuItem}`}>Blog</Link>
                 </li>
               </ul>
             </div>
           </nav>
         </header>
       </div>
-      <div className='row'>
-        <main>
-          {children}
-        </main>
+      <div className='row flex-grow-1'>
+        {children}
       </div>
     </div >
   )
