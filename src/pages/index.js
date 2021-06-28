5adabc2cbbdfbc64203a31c2e2ebb1ff4f8d6696
Marcus Sanatan <msanatan@gmail.com>
2021-06-27 23:39:03 -0400

    Layout homepage with jumbotron
    It's super simple, and I love ti

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,29 @@
 import * as React from 'react'
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+import { heroTitle, subtitle, heroName } from './index.module.css'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle='Home'>
-      <h1>Welcome to my site</h1>
+      <div className='container-fluid align-self-center'>
+        <div className='row justify-content-center '>
+          <div className='col-sm-4 d-flex justify-content-center py-5 py-md-0'>
+            <StaticImage
+              alt='Pixel avatar of Marcus'
+              src='../images/avatar.png'
+            />
+          </div>
+          <div className='col-sm-8 d-flex align-items-center'>
+            <div className='jumbotron'>
+              <div className='container'>
+                <h1 className={heroTitle}>Hi, I'm <span className={heroName}>Marcus Sanatan</span></h1>
+                <h3 className={subtitle}>I build webapps, games, content and communities.</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
