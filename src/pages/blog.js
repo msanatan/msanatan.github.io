eb8097dc172fd6a100b5534d5309f768689c92a8
Marcus Sanatan <msanatan@gmail.com>
2021-06-28 23:14:11 -0400

    Add list of blog posts from Markdown
    I'll create separate pages for StackAbuse and DigitalOcean.
    Needs some styling and pagination but it's functional!
    Taken from here:
    https://github.com/gatsbyjs/gatsby-starter-blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,33 +1,84 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
 import { pageTitle } from '../styles/page.module.css'
 
 const BlogPage = ({ data }) => {
+
+  const posts = data.allMarkdownRemark.nodes
+
+  if (posts.length === 0) {
+    return (
+      <Layout pageTitle='Blog'>
+        <p>
+          No blog posts found. Get to writing asap!
+        </p>
+      </Layout >
+    )
+  }
+
   return (
     <Layout pageTitle='Blog'>
       <h1 className={pageTitle}>Blog</h1>
-      <ul>
-        {
-          data.allFile.nodes.map(node => (
-            <li key={node.name}>
-              {node.name}
-            </li>
-          ))
-        }
-      </ul>
+      <ol style={{ listStyle: `none` }}>
+        {posts.map(post => {
+          const title = post.frontmatter.title || post.fields.slug
+
+          if (!post.frontmatter.link) {
+            return (
+              <li key={post.fields.slug}>
+                <article
+                  className="post-list-item"
+                  itemScope
+                  itemType="http://schema.org/Article"
+                >
+                  <header>
+                    <h2>
+                      <Link to={post.fields.slug} itemProp="url">
+                        <span itemProp="headline">{title}</span>
+                      </Link>
+                    </h2>
+                    <small>{post.frontmatter.date}</small>
+                  </header>
+                  <section>
+                    <p
+                      dangerouslySetInnerHTML={{
+                        __html: post.frontmatter.description || post.excerpt,
+                      }}
+                      itemProp="description"
+                    />
+                  </section>
+                </article>
+              </li>
+            )
+          }
+        })}
+      </ol>
     </Layout>
   )
 }
 
 export const query = graphql`
-  query AllBlogPosts {
-    allFile {
-      nodes {
-        name
+query AllBlogPosts {
+  site {
+    siteMetadata {
+      title
+    }
+  }
+  allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    nodes {
+      excerpt
+      fields {
+        slug
+      }
+      frontmatter {
+        title
+        link
+        date(formatString: "DD-MM-YYYY")
       }
     }
   }
+}
 `
 
 export default BlogPage
