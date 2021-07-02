73a8239418095be04b9bee2f32a6b11167189881
Marcus Sanatan <msanatan@gmail.com>
2021-07-02 00:50:27 -0400

    Showing 10 blog posts at a time is cooler, actually

--- gatsby-node.js
+++ gatsby-node.js
@@ -38,7 +38,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   }
 
   const posts = result.data.allMarkdownRemark.nodes
-  const postsPerPage = 5
+  const postsPerPage = 10
   const numPages = Math.ceil(posts.length / postsPerPage)
 
   // Create blog posts pages
