0495ba3d19be0426c0f90fa6f1c9e052bbda3758
Marcus Sanatan <msanatan@gmail.com>
2021-07-03 22:55:13 -0400

    Refactor layout to use SEO component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,9 @@
 import * as React from 'react'
 import { Link, graphql, useStaticQuery } from 'gatsby'
-import { Helmet } from 'react-helmet'
 import { menuItem } from './layout.module.css'
+import Seo from './seo'
 
-const Layout = ({ pageTitle, children }) => {
+const Layout = ({ pageTitle, description, meta, children }) => {
   const data = useStaticQuery(graphql`
   query GetBuildInfo {
     site {
@@ -15,30 +15,11 @@ const Layout = ({ pageTitle, children }) => {
   }
   `)
 
-  const siteTitleText = data.site.siteMetadata.title
-  const title = `${pageTitle} | ${siteTitleText}`
+  const title = `${pageTitle}`
 
   return (
     <div className='container d-flex flex-column min-vh-100'>
-      <Helmet
-        htmlAttributes={{
-          lang: 'en',
-        }}
-        bodyAttributes={{
-          className: 'h-100'
-        }}
-        title={title}
-        meta={[
-          {
-            property: `og:title`,
-            content: title,
-          },
-          {
-            property: `og:type`,
-            content: `website`,
-          },
-        ]}
-      />
+      <Seo title={title} description={description} meta={meta} />
       <header>
         <nav className='navbar navbar-expand-lg navbar-light'>
           <button className='navbar-toggler' type='button' data-bs-toggle="collapse" data-bs-target="#navBarTogglerMenu" aria-controls="navBarTogglerMenu" aria-expanded="false" aria-label="Toggle navigation">
