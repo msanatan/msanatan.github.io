6c6676f62d5ef769efd5ea60482024c5159ed124
Marcus Sanatan <msanatan@gmail.com>
2021-08-15 19:53:01 -0400

    Create a template for tags
    Admittedly, it's the same as the blog list.
    Maybe I should reuse the components...
    In any case, the new page query sorts this out.
    
    We also add some styles for a simple tag link

---
+++ src/templates/tagPage.js
@@ -0,0 +1,122 @@
+import * as React from 'react'
+import { Link, graphql } from 'gatsby'
+import Layout from '../components/layout'
+import BlogListButton from '../components/blogButton'
+import { slugify } from '../utils/slugify'
+import { pageTitle } from '../styles/page.module.css'
+import { postLink, tagLink } from './blogList.module.css'
+
+const TagsTemplate = ({ data, pageContext }) => {
+
+  const { currentPage, numPages, tag } = pageContext
+  const posts = data.allMarkdownRemark.nodes
+  console.log(posts)
+  const title = currentPage > 1 ? `${tag} pg. ${currentPage}` : `${tag}`
+
+  if (posts.length === 0) {
+    return (
+      <Layout title={title}>
+        <p>
+          Yikes! Can't find no blogs with that tag. Check back later!
+        </p>
+      </Layout >
+    )
+  }
+
+  let navButtons;
+  if (numPages !== 1) {
+    navButtons = (
+      <div className='row my-1'>
+        <div className='col-6 d-flex justify-content-end' >
+          {currentPage > 1 ? <BlogListButton text='Previous' link={currentPage > 2 ? `/tags/${tag}/${currentPage - 1}` : `/tags/${tag}`} /> : null}
+        </div >
+        <div className='col-6 d-flex justify-content-start'>
+          {currentPage < numPages ? <BlogListButton text='Next' link={`/tags/${tag}/${currentPage + 1}`} /> : null}
+        </div>
+      </div >
+    )
+  }
+
+  return (
+    <Layout title={title}>
+      <div className='container-fluid d-flex flex-column'>
+        <div className={`row ${pageTitle}`}>
+          <h1>{`Tags: ${tag}`}</h1>
+        </div>
+        <div className='row py-3'>
+          <div className='col-sm-8 offset-sm-2'>
+            <ol style={{ listStyle: `none` }}>
+              {posts.map(post => {
+                const title = post.frontmatter.title || post.fields.slug
+                const postTags = (
+                  <p>
+                    {post.frontmatter.tags.map((tag, i) => {
+                      return i < post.frontmatter.tags.length - 1
+                        ? (<span><Link to={`/tags/${slugify(tag)}`} itemProp="url" className={tagLink}>{tag}</Link>, </span>)
+                        : (<Link to={`/tags/${slugify(tag)}`} itemProp="url" className={tagLink}>{tag}</Link>)
+                    })}
+                  </p>
+                )
+                const postMetadata = post.frontmatter.updated ? `${post.frontmatter.date} (updated: ${post.frontmatter.updated})` : `${post.frontmatter.date}`
+
+                return (
+                  <li key={post.fields.slug} className='py-1'>
+                    <article
+                      className="post-list-item"
+                      itemScope
+                      itemType="http://schema.org/Article"
+                    >
+                      <header>
+                        <h2>
+                          <Link to={post.fields.slug} itemProp="url" className={postLink}>
+                            <span itemProp="headline">{title}</span>
+                          </Link>
+                        </h2>
+                        <small>{postMetadata}</small>
+                        {postTags}
+                      </header>
+                      <section>
+                        <p
+                          dangerouslySetInnerHTML={{
+                            __html: post.frontmatter.description || post.excerpt,
+                          }}
+                          itemProp="description"
+                        />
+                      </section>
+                    </article>
+                  </li>
+                )
+              })}
+            </ol>
+          </div>
+        </div>
+        {navButtons}
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query AllBlogPostsForTags($tag: String, $skip: Int!, $limit: Int!) {
+    allMarkdownRemark(
+      sort: {fields: [frontmatter___date], order: DESC }
+      filter: {frontmatter: {tags: {in: [$tag]}}},
+      limit: $limit
+      skip: $skip
+    ) {
+      nodes {
+        fields {
+          slug
+        }
+        frontmatter {
+          title
+          date(formatString: "YYYY-MM-DD")
+          updated(formatString: "YYYY-MM-DD")
+          tags
+        }
+      }
+    }
+  }
+`
+
+export default TagsTemplate
